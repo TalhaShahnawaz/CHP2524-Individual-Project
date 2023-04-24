c3a3d1c5e410db05f70a91cd4c42f3688c66a9d0
Talha Shahnawaz <U1977297@unimail.hud.ac.uk>
2023-04-24 18:50:37 +0500

    Minor changes done

--- Donation-Talha/database/migrations/2023_04_16_190922_create_patient_table.php
+++ Donation-Talha/database/migrations/2023_04_16_190922_create_patient_table.php
@@ -38,6 +38,6 @@ class CreatePatientTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('patient');
+        Schema::dropIfExists('patients');
     }
 }
